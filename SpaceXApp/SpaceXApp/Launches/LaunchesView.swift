b72428d43973d5f29bfb2f7621c3eef9a644ab19
a-elnajjar <code@mail.com>
2024-01-23 13:34:34 -0500

    updated the view and refactor

--- SpaceXApp/SpaceXApp/Launches/LaunchesView.swift
+++ SpaceXApp/SpaceXApp/Launches/LaunchesView.swift
@@ -8,22 +8,41 @@
 import SwiftUI
 
 struct LaunchesView: View {
-    @ObservedObject var viewModel:LaunchesViewModel = LaunchesViewModel()
-    
+    @ObservedObject var viewModel = LaunchesViewModel()
+
     var body: some View {
         NavigationView {
             ZStack {
-                List(self.viewModel.presenters) { item in
-                    LaunchesCell(presenters: item).onTapGesture(perform:{
-                        self.viewModel.itemSelected(at: item)})}.onAppear(perform: { self.viewModel.onAppear()})
-                    NavigationLink(
-                        destination: LaunchesDetailView(viewModel: self.viewModel.selectedViewModel),
-                        isActive:self.$viewModel.navigateToDetail,
-                        label:{ EmptyView()})
-                }.navigationBarTitle("Launches",displayMode:.inline)
+                launchesList
+                navigationLink
             }
+            .navigationBarTitle("Launches", displayMode: .inline)
         }
     }
+
+    private var launchesList: some View {
+        List(viewModel.presenters) { item in
+            LaunchesCell(presenters: item)
+                .onTapGesture {
+                    viewModel.itemSelected(at: item)
+                }
+        }
+        .onAppear {
+            viewModel.onAppear()
+        }
+    }
+
+    private var navigationLink: some View {
+        NavigationLink(
+            destination: LaunchesDetailView(viewModel: viewModel.selectedViewModel),
+            isActive: $viewModel.navigateToDetail,
+            label: {
+                EmptyView()
+            }
+        )
+    }
+}
+
 
 
 struct ContentView_Previews: PreviewProvider {
